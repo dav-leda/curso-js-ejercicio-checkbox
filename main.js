ff01112913126516bef6fae19797b720817a7d24
David Leda <davmail99@yahoo.com>
2022-04-27 17:37:35 -0300

    1st commit

---
+++ main.js
@@ -0,0 +1,60 @@
+
+
+const select = element => document.querySelector(element);
+const selectAll = elements => document.querySelectorAll(elements);
+
+const mostrarSeleccionados = select('#seleccionados');
+const alerta = select('#alerta');
+
+const equipos = [
+  'River',
+  'Boca',
+  'Racing',
+  'Independiente',
+  'San Lorenzo',
+  'Velez',
+  'Newells',
+  'Lanus',
+  'Estudiantes',
+  'Ferro',
+  'Huracan',
+  'Gimnasia',
+  'Banfield',
+  'Talleres'
+]
+
+
+for(let equipo of equipos) {
+  
+  const formulario = select('#formulario');
+  
+  formulario.innerHTML += `
+  <input type="checkbox" id="${equipo}" name="${equipo}" value="${equipo}">
+  <label for="${equipo}">${equipo}</label><br>
+  `;
+}
+
+const checkboxes = selectAll('input[type=checkbox]');
+
+checkboxes.forEach( checkbox => {
+
+  checkbox.addEventListener('change', (e) => {
+
+    const seleccionados = selectAll('input[type=checkbox]:checked');
+
+    if (seleccionados.length < 5) {
+
+      alerta.innerText = '';
+      mostrarSeleccionados.innerHTML = '';
+      
+      seleccionados.forEach( equipo => {
+        mostrarSeleccionados.innerHTML += `<li>${equipo.value}</li>`;
+      })
+
+    } else {
+      e.target.checked = false;
+      alerta.innerText = 'No podés elegir más de 4 equipos';
+    }
+  })
+})
+
